fe0f6a1ff71f2bd72fc19222d4a679fe6a1bd604
Alec Stephens <astephens91@gmail.com>
2019-08-14 09:55:26 -0400

    Attempt at stacking loops

--- nested.py
+++ nested.py
@@ -3,13 +3,64 @@
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "Your Github Username"
+__author__ = "astephens91"
 
 import sys
 
-def main(args):
-    """Add your code here"""
+def areBracketsNestedCorreclty(filename):
+    fwrite = open("output.txt", "a+")
     
+    fread = open(filename, "r")
+    text =fread.read()
+    
+    stack = []
+
+
+    for i in range(len(text)):
+        if text[i] == '(' and text[i+1] == '*':
+            stack.append(text[i] + text[i+1])
+        elif (text[i] == '(' or text[i] == '[' or text[i] == '{'):
+            stack.append(text[i])
+            continue
+            
+        if (len(stack) == 0):
+            fwrite.write("YES" + "\n")
+        
+        if text[i] == '*' and text[i+1] == ')':
+            x = stack.pop()
+
+            if (x == '(' or x == '[' or x == '{'):
+                fwrite.write("NO " + str(text.index(text[i])) + "\n")
+
+        elif text[i] == ')' and text[i-1] != '*':
+            x = stack.pop()
+        
+            if (x == '{' or x == '[' or x =='(*'):
+                fwrite.write("NO " + str(text.index(text[i])) + "\n")
+        
+        elif text[i] == '}':
+            x = stack.pop()
+            
+            if (x == '(' or x == '[' or x== '(*'):
+                fwrite.write("NO " + str(text.index(text[i])) + "\n")
+        
+        elif text[i] == ']':
+            x = stack.pop()
+
+            if (x == '(' or x == '{' or x == '(*'):
+                fwrite.write("NO " + str(text.index(text[i])) + "\n")
+    if len(stack):
+        fwrite.write("YES" + "\n")
+    
+    
+
+
+
+
+def main(args):
+   
+    areBracketsNestedCorreclty("input.txt")
+    """Add your code here"""
 
 if __name__ == '__main__':
     main(sys.argv)
